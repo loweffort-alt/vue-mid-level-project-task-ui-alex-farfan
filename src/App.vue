030d4824f92a6de15ac81e67af4e7d22ae4146cd
loweffort-alt <farfan_alexander@outlook.com>
2025-05-09 18:15:13 -0500

    New Element: Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <nav class="justify-center items-center">
+      <h1>Welcome to my technical test</h1>
+      <div class="flex gap-8">
+        <router-link to="/">Home</router-link>
+        <router-link to="/projects">Projects</router-link>
+        <router-link to="/projects/1/tasks">Tasks</router-link>
+      </div>
+      <h2>Developed by Alex Farfan Navarro</h2>
+    </nav>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <router-view />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+nav {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+nav h1 {
+  font-size: 2rem;
+  color: #42b983;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+nav h2 {
+  font-size: 1.5rem;
 }
 </style>
